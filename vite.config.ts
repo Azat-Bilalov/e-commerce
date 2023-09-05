70323d78207823d17655a3325b64427bd938b7ff
Azat-Bilalov <52991059+Azat-Bilalov@users.noreply.github.com>
2023-09-06 00:34:40 +0300

    Сетап с линтером и алиасами

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+      '@pages': '/src/App/pages',
+      '@utils': '/src/utils',
+      '@configs': '/src/configs',
+    },
+  },
+})
